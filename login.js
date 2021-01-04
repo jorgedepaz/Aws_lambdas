96b2eceb8b378db6eb3bce3f5d7131f184e12f09
Jorge <alejandro131093@gmail.com>
2021-01-03 22:16:24 -0600

    x

--- login.js
+++ login.js
@@ -9,9 +9,32 @@ module.exports.login = (event, context, callback) =>{
 
     context.callbackWaitsForEmptyEventLoop = false;
     const body = JSON.parse(event.body);
-    
+
     var payload = {};
     var signOptions = {};
+    
+    var usuario = {
+      idusuario: null,
+      username: null,
+      password: null
+    };
+
+    var empleado = {
+      idsucursal: null,
+      nombre: null
+    };
+
+    var rol = {
+      idrol: null,
+      nombre: null
+    };
+
+    var rol_permisos = [];
+
+    var permisos = {
+      idpermiso: null,
+      nombre: null
+    };
 
     var rules = {
       username: 'required|string|max:45',
@@ -30,8 +53,12 @@ module.exports.login = (event, context, callback) =>{
   
       console.log("Este es el usuario: ");
       console.log(User);
-      
-      connection.query('SELECT * FROM pos.usuario WHERE username = ?',[User.username], function (error, results, fields) { //query para insertar el documento venta
+       connection.beginTransaction(function (err) {
+        var idG;
+        if (err) {
+          throw err;
+        }
+      connection.query('SELECT * FROM pos.usuario WHERE username = ?',[User.username], function (error, results, fields) { //query para consultar el usuario
         if (error) {
           return connection.rollback(function () {
             throw error;
@@ -40,12 +67,25 @@ module.exports.login = (event, context, callback) =>{
             console.log("Resultados");
             console.log(results[0]);
             
-
           if(results != ""){
+            //Armando objeto usuario para el payload
+            usuario.idusuario = results[0].idusuario;
+            usuario.username = results[0].username;
+            
+            console.log(usuario);
 
             bcrypt.compare(User.password, results[0].password, function(err, result) {
                 if (result) {
                     //Codigo para generar el token
+                    let sqlEmpleado = "";//consulta para obtener el objeto que corresponde al empleado
+                    connection.query(sqlEmpleado,[usuario.idusuario], function (error, results, fields) { //query para consultar empleado
+                      if (error) {
+                        return connection.rollback(function () {
+                          throw error;
+                        });
+                      }else{
+                        //crear aqui el objeto empleado con todos sus datos
+                        //empleado.
                     payload = {
                         username: results[0].username
                     }
@@ -54,8 +94,16 @@ module.exports.login = (event, context, callback) =>{
                      };
                      
                     jwt.sign(payload,config.SECRET_TOKEN,signOptions, function(error,token){
-                        if (error) {
-                            callback(null, {
+                        
+                      if (error) {//si existe error al generar el token
+                          connection.commit(function (err) { //inicio del commit
+                            console.log("Mensaje desde el commit");
+                            if (err) {
+                              return connection.rollback(function () {
+                                throw err;
+                              });
+                            } else {
+                              callback(null, {
                                 statusCode: 500,
                                 headers: {
                         
@@ -68,8 +116,19 @@ module.exports.login = (event, context, callback) =>{
                                     message: error
                                 })
                               });
+                            } //final del else
+                          }) //final del commit
+                            
                         }else{
-                            callback(null, {
+
+                          connection.commit(function (err) { //inicio del commit
+                            console.log("Mensaje desde el commit");
+                            if (err) {
+                              return connection.rollback(function () {
+                                throw err;
+                              });
+                            } else {
+                                 callback(null, {
                                 statusCode: 200,
                                 headers: {
                         
@@ -83,11 +142,25 @@ module.exports.login = (event, context, callback) =>{
                                     token
                                 })
                               });
-                        }
-                    });
-                            
-                }else{
-                    callback(null, {
+                            } //final del else
+                          }) //final del commit
+                       
+                        }//final del else cuando se genera el token con exito
+                    });//Final del jwt sign
+
+                   }//final del else para consultar empleado
+                 })//final de la query para consultar empleado
+
+                }//final del if para comparar el resultado que arroja bcrypt
+                else{
+                  connection.commit(function (err) { //inicio del commit
+                    console.log("Mensaje desde el commit");
+                    if (err) {
+                      return connection.rollback(function () {
+                        throw err;
+                      });
+                    } else {
+                      callback(null, {
                         statusCode: 500,
                         headers: {
                 
@@ -100,11 +173,21 @@ module.exports.login = (event, context, callback) =>{
                             message: "Contraseña incorrecta"
                         })
                       }); 
+                    } //final del else
+                  }) //final del commit
+                 
                 }
             });
              
             }else{
-                callback(null, {
+              connection.commit(function (err) { //inicio del commit
+                console.log("Mensaje desde el commit");
+                if (err) {
+                  return connection.rollback(function () {
+                    throw err;
+                  });
+                } else {
+                  callback(null, {
                     statusCode: 500,
                     headers: {
             
@@ -117,14 +200,34 @@ module.exports.login = (event, context, callback) =>{
                         message: "No existe un usuario con ese nombre"
                     })
                   }); 
+                } //final del else
+              }) //final del commit 
             }
         }
         
-      });//final de la query
+      });//final de la query para consultar el usuario
+    });//final de la transaccion 
+  
+    }//final del if para la validacion
+    else{
+      callback(null, {
+        statusCode: 500,
+        headers: {
+  
+          'Access-Control-Allow-Origin': '*',
+  
+          'Access-Control-Allow-Credentials': true,
+  
+        },
+        body: JSON.stringify({
+          message: 'Datos incorrectos'
+        })
+      })
+    }
+  
+    }//Final de la funcion
 
-      
-
-      /*(async function() {//Funcion (IIFE) ‘immediately invoked function expression’
+    /*(async function() {//Funcion (IIFE) ‘immediately invoked function expression’
   
         newUser.password =  await helpers.encryptPassword(newUser.password);
         console.log("Este es el nuevo usuario: ");
@@ -158,24 +261,3 @@ module.exports.login = (event, context, callback) =>{
         });//final de la query
   
       })();*/
-      
-      
-  
-    }//final del if para la validacion
-    else{
-      callback(null, {
-        statusCode: 500,
-        headers: {
-  
-          'Access-Control-Allow-Origin': '*',
-  
-          'Access-Control-Allow-Credentials': true,
-  
-        },
-        body: JSON.stringify({
-          message: 'Datos incorrectos'
-        })
-      })
-    }
-  
-    }//Final de la funcion
