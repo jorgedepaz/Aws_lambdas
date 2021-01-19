5014bacae5fa440c9725b020d09967668f1a032d
Jorge <alejandro131093@gmail.com>
2021-01-19 16:56:51 -0600

    Comentarios logica alternativa

--- auth_token.js
+++ auth_token.js
@@ -1,39 +1,84 @@
 'use strict';
-//const config = require('../config');
-//const _ = require('lodash');
-
-//const jwt = require('jsonwebtoken');
+const config = require('../config');
+const _ = require('lodash');
+const jwt = require('jsonwebtoken');
 
 //arn:aws:lambda:us-east-1:375194658163:function:sales-orders-shop-dev-
-//al final del arn se agrega el nombre del la funcionpo/* */
+//al final del arn se agrega el nombre del la funcionpo
 
-/*const authorizeUser = (userScopes, methodArn) => {
-    //const hasValidScope = _.some(userScopes, scope => _.endsWith(methodArn, scope));
+const authorizeUser = (userScopes, methodArn) => {
     const hasValidScope = _.some(userScopes, scope => _.endsWith(methodArn, scope));
+    //const hasValidScope = _.some(userScopes, scope => _.endsWith(methodArn, scope));
+
+//Logica alternativa    
+    /*for (let index = 0; index < userScopes.length; index++) {
+        //const element = array[index];
+        if (_.endsWith(methodArn, userScopes[index])) {
+            return true;      
+        }
+        
+    }
+    return false;*/
+//Fin logica alternativa
+
     return hasValidScope;
-  };*/
+  };
 
 module.exports.auth_token = async(event, context)=>{
-
-    //const tokenID= (event.headers && (event.headers['X-Amz-Security-Token'] || event.headers['x-amz-security-Token'])) || event.authorizationToken;
-
-    //const tokenID= event.headers['X-Amz-Security-Token'];
-    const tokenID= event.headers['Authorization'];
+    const authorizerToken = event.headers['Authorization'];
+    const authorizerArr = authorizerToken.split(' ');
+    const token = authorizerArr[1];
     
-    console.log("Este es el token");
-    console.log(tokenID);
+    
 
-   if (!tokenID) {
-        console.log("No se encuentra el token en el evento");
-        //return generatePolicy({ allow: false });
+    if (authorizerArr.length != 2 || authorizerArr[0] != 'Bearer' || authorizerArr[1].length == 0) {
+        console.log('Primer IF');
         return generatePolicy({ allow: false });
-        //console.log(generatePolicy('undefined','Deny',event.methodArn));
-        //return generatePolicy('undefined','Deny',event.methodArn);
+    }
+    var decodeJwt;
+
+    try {
+        decodeJwt = jwt.verify(token,config.SECRET_TOKEN); 
+        //console.log(decodeJwt);   
+    } catch(error) {
+        
+        return generatePolicy({ allow: false });    
+    }
+
+    
+    if (typeof decodeJwt.usuario != 'undefined' && decodeJwt.usuario.username.length > 0) {
+
+               
+        let permisos = _.map(decodeJwt.rol_permisos, 'nombre');
+        
+        console.log("Estos son los permisos");
+        console.log(permisos);
+        console.log("Este es el ARN");
+        console.log(event.methodArn);
+
+        const isAllowed = authorizeUser(permisos, event.methodArn);
+      /*  for (let index = 0; index < permisos.length; index++) {
+            //const element = array[index];
+            if (_.endsWith(event.methodArn, permisos[index])) {
+                console.log(_.endsWith(event.methodArn, permisos[index]));
+                return generatePolicy({ allow: true });
+            }
+            
+        }*/
+        
+        //const effect = isAllowed ? 'true' : 'false';
+        //console.log("Is allowed?");
+        //console.log(isAllowed);
+        //console.log("Effect?");
+        //console.log(effect);
+        //console.log("Objeto");
+
+        //la parte de abajo descomentarla si no esta el for
+        console.log({ allow: isAllowed });
+        return generatePolicy({ allow: isAllowed });
     }else{
-        console.log("Token encontrado, allow: true");
-        //console.log(generatePolicy('undefined','Allow',event.methodArn));
-        return generatePolicy({ allow: true });
-        //return generatePolicy('undefined','Allow',event.methodArn);
+        console.log('Por defecto');
+        return generatePolicy({ allow: false });
     }
 
 }
